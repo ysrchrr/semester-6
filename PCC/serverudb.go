ccc929d74fab2c3b87ad346ed763de83782633c2
Muhammad Yasir Choiri <ysrchr@gmail.com>
2021-03-05 20:02:54 +0700

    Add PCC: Add Profil, Progdi Page

--- PCC/serverudb.go
+++ PCC/serverudb.go
@@ -2,11 +2,40 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type halaman struct {
+	Judul     string
+	Deskripsi string
+	Progdi    []jurusan
+}
+
+type jurusan struct {
+	Jenjang string
+	Nama    string
+}
+
 func profil(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Halaman Profil")
+}
+
+func progdi(w http.ResponseWriter, r *http.Request) {
+	var tampile, salahe = template.ParseFiles("template.html")
+	if salahe != nil {
+		fmt.Println(salahe.Error())
+		return
+	}
+	var data = halaman{
+		Judul:     "Program studi",
+		Deskripsi: "Fakultas Ilmu Komputer",
+		Progdi: []jurusan{
+			{Jenjang: "D3", Nama: "MI"},
+			{Jenjang: "S1", Nama: "SI"},
+		},
+	}
+	tampile.Execute(w, data)
 }
 
 func main() {
@@ -15,6 +44,7 @@ func main() {
 	})
 
 	http.HandleFunc("/profil", profil)
+	http.HandleFunc("/progdi", progdi)
 
 	fmt.Println("Server running di: http://localhost:8182")
 	http.ListenAndServe(":8182", nil)
